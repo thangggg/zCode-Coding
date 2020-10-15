65ececd079aef06aeef41c93b17c16da7eec02cb
thangggg <thangtran.ptit@gmail.com>
2020-10-15 10:51:50 +0700

    12. Loai bo xau con

--- 12.STRING01.cpp
+++ 12.STRING01.cpp
@@ -4,15 +4,9 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    string s,x; getline(cin, s); cin >> x;
-    istringstream tk(s); vector<string> v;
-    while(tk >> s) v.push_back(s);
-    for(auto i = v.begin(); i != v.end(); ++i){
-        if ( *i == x){
-            v.erase(i);
-            break;
-        } 
-    } 
-    for(auto i : v) cout << i << " ";
+    string s,sub;
+    getline(cin, s); cin >> sub;
+    s.erase(s.find(sub), sub.size());
+    cout << s;
     return 0;
 }
