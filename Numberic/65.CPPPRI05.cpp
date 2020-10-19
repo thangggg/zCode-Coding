11b542d5ee22ec1a8a150c2d117ebbc2fdefc34d
thangggg <thangtran.ptit@gmail.com>
2020-10-19 16:00:58 +0700

    66. Prime 6

---
+++ Numberic/65.CPPPRI05.cpp
@@ -0,0 +1,27 @@
+//Cre: thangggg
+#include <bits/stdc++.h>
+using namespace std;
+
+const int sMAX = 1e5 + 1; 
+bitset<sMAX> p;  
+
+void sieve(){
+    p.set(); // 1
+    p[0] = 0; p[1] = 0;
+    for(int i = 2; i * i <= sMAX; ++i)
+        if (p[i])
+            for(int j = i * i; j <= sMAX; j += i) 
+                p[j] = 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t; cin >> t; sieve();
+    while(t--){
+        int m, n; cin >> m >> n;
+        for(int i = m; i <= n; ++i)
+            if ( p[i] ) cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
